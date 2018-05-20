d3a310d4147650aa777a019d16ce275be55b5bb1
rjsgmlgggg@gmail.com <rjsgmlgggg@gmail.com>
2018-05-21 00:42:47 +0900

    짜잘한 수정

--- predict/needs/jsonProcess.py
+++ predict/needs/jsonProcess.py
@@ -2,13 +2,13 @@ from django.http import HttpResponse
 import json
 import base64
 
-# decode Json type data
+# Json 타입 디코딩
 def decode_json(request, json_field):
     json_data = json.loads(request.body.decode('utf-8'))
     json_data = json_data[json_field]
     return base64.b64decode(json_data)
 
-# encode Json
+# Json 타입 인코딩
 def encode_json(target):
     result = dict()
     if len(target) != 0:
--- predict/needs/processData.py
+++ predict/needs/processData.py
@@ -6,7 +6,7 @@ def process_result_dict(result, size):
     temp_result = result
     sorted_list = list()
     
-    # if you want to resize 'size', consider Unity Burpy Project!
+    # 결과 List 사이즈를 조절할 수 있습니다. 다만 Unity 프로젝트의 변수도 고려되어야 합니다.
     for i in range(size):
         highest_predict_key = max(temp_result.keys(), key=(lambda k: temp_result[k]))
         highest_predict_value = temp_result[highest_predict_key]
